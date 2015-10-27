9d13ebb5ecd1090a0d47eec275c0b93f7473914a
zang0702 <abcsdy159456@yahoo.com.tw>
2015-10-27 13:31:06 +0800

    hk2

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.ok = New System.Windows.Forms.Button()
+        Me.out = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(157, 77)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(81, 22)
+        Me.txtusername.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(73, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "帳號"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label2.Location = New System.Drawing.Point(73, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "密碼"
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(156, 142)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(82, 22)
+        Me.txtpassword.TabIndex = 3
+        '
+        'ok
+        '
+        Me.ok.Font = New System.Drawing.Font("微軟正黑體", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.ok.Location = New System.Drawing.Point(97, 213)
+        Me.ok.Name = "ok"
+        Me.ok.Size = New System.Drawing.Size(70, 34)
+        Me.ok.TabIndex = 4
+        Me.ok.Text = "登入"
+        Me.ok.UseVisualStyleBackColor = True
+        '
+        'out
+        '
+        Me.out.Font = New System.Drawing.Font("微軟正黑體", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.out.Location = New System.Drawing.Point(204, 213)
+        Me.out.Name = "out"
+        Me.out.Size = New System.Drawing.Size(71, 34)
+        Me.out.TabIndex = 5
+        Me.out.TabStop = False
+        Me.out.Text = "離開"
+        Me.out.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(369, 317)
+        Me.Controls.Add(Me.out)
+        Me.Controls.Add(Me.ok)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtusername)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtusername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtpassword As TextBox
+    Friend WithEvents ok As Button
+    Friend WithEvents out As Button
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load, txtusername.TextChanged, txtpassword.TextChanged
+
+    End Sub
+
+    Private Sub ok_Click(sender As Object, e As EventArgs) Handles ok.Click
+        If (String.IsNullOrEmpty(txtusername.Text)) Then
+            txtusername.BackColor = Color.MistyRose
+            txtpassword.BackColor = Color.MistyRose
+        End If
+
+        If txtusername.Text = "login" Then
+            txtusername.BackColor = Color.MistyRose
+        End If
+
+        If txtpassword.Text = "abc123" Then
+            txtpassword.BackColor = Color.MistyRose
+        End If
+
+    End Sub
+
+    Private Sub ok_Click_1(sender As Object, e As EventArgs) Handles ok.Click
+
+        If txtusername.Text = "login" Then
+            MessageBox.Show("帳號密碼輸入正確！")
+        End If
+
+        If txtpassword.Text = "abc123" Then
+            MessageBox.Show("帳號密碼輸入正確！")
+
+        End If
+
+
+    End Sub
+
+    Private Sub out_Click(sender As Object, e As EventArgs) Handles out.Click
+        End
+    End Sub
+End Class
